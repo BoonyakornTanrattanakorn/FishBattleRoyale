3bde87aecc07aef358c1e50b6c63acf43ed8cb05
BoonyakornTanrattankorn <boonyakorn.tan@gmail.com>
2026-02-13 21:21:39 +0700

    feat: implement grid movement

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -1,46 +1,55 @@
+# see: https://kidscancode.org/godot_recipes/4.x/2d/grid_movement/index.html
+
 extends Area2D
 
 class_name Player
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var bomb_placement_system: BombPlacementSystem = $BombPlacementSystem
-@onready var raycasts: Raycasts = $RayCasts
+@onready var ray = $RayCast2D
 @onready var power_up_system: Node = $PowerUpSystem
 
-@export var movement_speed: float = 200
+var animation_speed = 3
+var moving = false
 
 var hp = 3
 var max_bombs = 1
-var movement: Vector2 = Vector2.ZERO
 
 const MAX_HP = 3
 
-func _process(delta: float) -> void:
-	var collision = raycasts.check_collisions()
-	
-	if collision.has(movement):
-		return
-	position += movement * delta * movement_speed
+func _ready() -> void:
+	position = position.snapped(Vector2.ONE * Config.tile_size)
+	position += Vector2.ONE * Config.tile_size/2
 
 func _input(_event: InputEvent) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if moving:
+		return
 		
-	if Input.is_action_pressed("Right"):
-		movement = Vector2.RIGHT
-	elif Input.is_action_pressed("Left"):
-		movement = Vector2.LEFT
-	elif Input.is_action_pressed("Up"):
-		movement = Vector2.UP
-	elif Input.is_action_pressed("Down"):
-		movement = Vector2.DOWN
-	else:
-		movement = Vector2.ZERO
+	if Input.is_action_just_pressed("Right"):
+		move(Vector2.RIGHT)
+		animated_sprite_2d.flip_h = false
+	elif Input.is_action_just_pressed("Left"):
+		move(Vector2.LEFT)
+		animated_sprite_2d.flip_h = true
+	elif Input.is_action_just_pressed("Up"):
+		move(Vector2.UP)
+	elif Input.is_action_just_pressed("Down"):
+		move(Vector2.DOWN)
 	
 	if Input.is_action_just_pressed("PlaceBomb"):
 		bomb_placement_system.place_bomb()
-		
 
+func move(dir):
+	ray.target_position = dir * Config.tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var tween = create_tween()
+		tween.tween_property(self, "position",
+			position + dir *    Config.tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
+		
 func reduceHp():
 	setHp(getHp()-1)
 	if getHp() <= 0:
