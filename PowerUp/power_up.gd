c7569513a836f5c60373e4b25468f7b49190c902
jednipit-golf <jednipit_golf@hotmail.com>
2026-02-24 18:58:02 +0700

    power up UI fix

--- PowerUp/power_up.gd
+++ PowerUp/power_up.gd
@@ -9,3 +9,10 @@ var type: Utils.PowerUpType
 func init(power_up_res: PowerUpRes):
 	sprite_2d.texture = power_up_res.texture
 	type = power_up_res.type
+	
+	# Scale up HP icon since it's smaller than other power-ups
+	if type == Utils.PowerUpType.HP:
+		sprite_2d.scale = Vector2(2, 2)
+	elif type == Utils.PowerUpType.SPEED_UP:
+		sprite_2d.scale = Vector2(1.5, 1.5)
+		
